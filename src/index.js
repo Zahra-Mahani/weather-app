50890378d6e8f1056d8684cb5c4a0579921d3932
Zahra Mahani <80246220+Zahra-Mahani@users.noreply.github.com>
2025-09-29 13:08:08 +0330

    added api and degree result to search function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function updateResult(city) {
+  let apiKey = "12e40o825c4b3b48ba032531ct05303f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshDegree);
+}
+function refreshDegree(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let resultCity = document.querySelector("#city-result");
+  resultCity.innerHTML = response.data.city;
+}
 function searchResult() {
   event.preventDefault();
   let cityElement = document.querySelector("#city");
-  let resultCity = document.querySelector("#city-result");
-  resultCity.innerHTML = cityElement.value;
+
+  updateResult(cityElement.value);
 }
 let formElement = document.querySelector("#city-form");
 formElement.addEventListener("submit", searchResult);
